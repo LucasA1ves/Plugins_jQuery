516293cf35d4228ed45a5ba8f999aa110d71a79b
Lucas Alves <lacsa2014@gmail.com>
2024-09-13 19:33:18 -0300

    Adicionada Validação

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
+// Aplicar máscaras nos campos
+$('#telefone').mask('(00) 00000-0000', {
+  placeholder: '(DDD) 12345-6789'
 })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
+$('#cpf').mask('000.000.000-00', {
+  placeholder: '123.456.789-00'
 })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
+$('#cep').mask('00000-000', {
+  placeholder: '01234-567'
 })
 
 $('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
+  rules: {
+    nome: {
+      required: true
     },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+    email: {
+      required: true,
+      email: true
     },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    telefone: {
+      required: true
+    },
+    endereco: {
+      required: true
+    },
+    cep: {
+      required: true
+    },
+    cpf: {
+      required: true
     }
+  },
+  submitHandler: function (form) {
+    alert('Sua requisição foi enviada para análise, parabéns pela aquisição!')
+    form.reset() // Reseta o formulário após o envio
+  },
+  invalidHandler: function (event, validator) {
+    alert('Por favor, preencha os campos para prosseguir com a compra!')
+  }
 })
